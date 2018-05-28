9cc4ced54153f6781e6db7e72069baacea2eb692
cd C:\RailsInstaller\Ruby2.2.0\bin <copy rake.bat rails.bat>
2018-05-28 17:11:13 +0200

    Poprawka

--- todo.js
+++ todo.js
@@ -1,31 +1,33 @@
 function addItem() {
-   var task = document.createElement("div");
-   task.innerHTML = document.getElementById("task").value;
-   taskItem = document.createElement("div");
-   taskItem.innerHTML = '<span>' + task.innerHTML + '</span>' + '<button>Remove</button>';
-   taskItem.className = 'task';
-   document.getElementById("taskList").appendChild(taskItem);
+  var task = document.createElement("div");
+  task.innerHTML = document.getElementById("task").value;
+  taskItem = document.createElement("div");
+  taskItem.innerHTML = '<span>' + task.innerHTML + '</span>' + '<button onClick="removeItem()">Remove</button>';
+  taskItem.className = 'task';
+  document.getElementById("taskList").appendChild(taskItem);
+  saveList();
+}
 
-   saveList();
-  }
-
-
-  function removeItem() {
+function removeItem() {
+  if (confirm("Czy na pewno chcesz usunąć?")) {
    var removeTask = document.getElementById("taskList");
    removeTask.removeChild(this);
    saveList();
-  }
+ }
+}
 
-  function saveList() {
-   localStorage.storedList = document.getElementById("taskList").innerHTML;
-  }
-  function loadList() {
-   document.getElementById("taskList").innerHTML = localStorage.storedList;
-   for(var i = 0; i < list.children.length; i++) {
-    list.children[i].onclick = removeItem;
-   }
-  }
+function saveList() {
+  localStorage.storedList = document.getElementById("taskList").innerHTML;
+}
 
-  if(localStorage.storedList) {
+function loadList() {
+  let taskListElement = document.getElementById('taskList');
+  taskListElement.innerHTML = localStorage.storedList;
+  for (let i = 0; i<taskListElement.children.length; i++) {
+    taskListElement.children[i].onclick = removeItem;
+  }
+}
+
+if(localStorage.storedList) {
    loadList();
-  }
+}
